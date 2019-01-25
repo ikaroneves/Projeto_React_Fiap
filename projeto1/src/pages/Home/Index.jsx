1dc69faaa12ea1068e2d9e513b56cdf77532cfe7
Ikaro Neves <ikaro.neves@outlook.com>
2019-01-24 23:08:14 -0200

    adicionado DIV com a busca para a API

--- projeto1/src/pages/Home/Index.jsx
+++ projeto1/src/pages/Home/Index.jsx
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class Home extends React.Component {
     render() {
-        return(<div> Home </div>)
-    }
+        return(<div>
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input className="mdl-textfield__input" type="text" id="search" />
+                    <label className="mdl-textfield__label" htmlFor="search">Digite para buscar...</label>                
+                </div>
+                </div>)
+            }
 }
 
 export default Home;
